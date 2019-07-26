e64ca224d6d7db208b1ea139836d397f0851ac42
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-26 16:56:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |attendee|
+  attendees.map do |attendee|
     return "Hello, my name is #{attendee}."
   end
 end
