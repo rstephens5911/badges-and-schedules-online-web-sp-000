57bd827077380660e31e4bb8205d7423ecc40f1d
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-26 16:48:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,6 @@
   # Write your code here.
     attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-  def badge_maker(name)
-    puts "Hello, my name is #{name}."
-  end
+def badge_maker(name)
+  return "Hello, my name is #{name}."
 end
