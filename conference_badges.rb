75cb007c6f4d45645afc263a5672e1d1c2e9c7e4
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-26 17:00:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,12 +2,12 @@
     attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
   attendees.each do |attendee|
-    return "Hello, my name is #{attendee}."
+    "Hello, my name is #{attendee}."
   end
 end
 
