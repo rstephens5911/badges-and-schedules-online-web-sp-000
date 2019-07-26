3bfd5e06e14f86d1458d752c3398cebc3743fc7b
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-26 16:45:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,24 +5,3 @@
     puts "Hello, my name is #{name}."
   end
 
-  def batch_badge_creator(attendees)
-    attendees.each do |element|
-      "Hello, my name is #{element}."
-    end
-  end
-
-  def assign_rooms(attendess)
-    room_array = []
-    attendees.each_with_index do |attendee, index|
-      room_array.push("Hello, #{attendee}! You'll be assigned to room #{index + 1}!")
-    end
-  end
-
-  def printer(attendees)
-    batch_badge_creator(attendees).each do |badge|
-      puts badge
-    end
-    assign_rooms(attendees).each do |room|
-      puts room
-    end
-  end
