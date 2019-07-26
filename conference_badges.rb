cfaed0aff0a59fd26df6b7ad6028a72d1c9a80e4
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-26 16:30:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,21 +1,22 @@
-# Write your code here.
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  # Write your code here.
+    attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(name)
-  puts " Hello, my name is #{name}"
-end
-
-def batch_badge_creator(attendess)
-  attendees.each do |element|
-    badge_maker(element)
+  def badge_maker(name)
+    puts " Hello, my name is #{name}"
   end
-  return badge_messages_array
-end
 
-def assign_room(attendess)
-  room_array = []
-  array.each_with_index {|name, index|}
-    room_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
+  def batch_badge_creator(attendess)
+    attendees.each do |element|
+      badge_maker(element)
+    end
+    return badge_messages_array
   end
-  return room_array
+
+  def assign_room(attendess)
+    room_array = []
+    array.each_with_index {|name, index|}
+      room_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
+    end
+    return room_array
+  end
 end
